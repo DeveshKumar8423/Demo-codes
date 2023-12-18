0df9b1e16cc6eff7d9c40f003c0d4c3aae5a25a6
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2023-12-18 20:50:53 +0530

    largest element in array

---
+++ n.py
@@ -0,0 +1,12 @@
+def largest(arr, n):
+
+	max = arr[0]
+	for i in range(1, n):
+		if arr[i] > max:
+			max = arr[i]
+	return max
+
+arr = [1, 2, 3, 4, 5]
+n = len(arr)
+Ans = largest(arr, n)
+print("Largest in given array ", Ans)
