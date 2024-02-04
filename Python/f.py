4aa0beaa4dce006ed565374c24062e3a0afeaeca
DeveshKumar8423 <deveshkumar8423gola@gmail.com>
2024-02-04 23:23:13 +0530

    print element of list

---
+++ Python/f.py
@@ -0,0 +1,10 @@
+#print element of list
+list = [2, 3, 4, 5, 6, 7, 8, 9, 0]
+i = 0
+while i < len(list):
+    element = list[i] 
+    print(element)
+    i = i+1
+    
+else:
+    print("Condition false")
